a414521989d0dc838a75492ef62ed01a2b9ccc45
PyroPythonTV <pyropython08@gmail.com>
2017-02-05 05:18:40 -0500

    loading screen

---
+++ teeload/config.php
@@ -0,0 +1,11 @@
+<?php
+
+ /* SET PASSWORD HERE
+	THIS IS IMPORTANT
+	SO YOU CAN ACCESS
+	THE ADMIN PAGE :) */
+	
+	$password = "xxxx"; // Replace xxx with anything. letters, numbers and symbols are all allowed.
+						// Login at yourdomain.com/teeload/adminlogin.php
+
+?>
